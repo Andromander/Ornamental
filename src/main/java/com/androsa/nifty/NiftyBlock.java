a5ab006bc215bb83cfa0832d6d4d21eb47383eb7
Andromander <cathy@feodoroff.com>
2019-04-09 20:59:54 +1000

    Slabs are really dumb, but fixed #14

--- src/main/java/com/androsa/nifty/NiftyBlock.java
+++ src/main/java/com/androsa/nifty/NiftyBlock.java
@@ -3,26 +3,29 @@ package com.androsa.nifty;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+
+import java.util.function.Supplier;
 
 public enum NiftyBlock {
-    IRON(Material.IRON, MapColor.IRON, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 1),
-    GOLD(Material.IRON, MapColor.GOLD, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2),
-    DIAMOND(Material.IRON, MapColor.DIAMOND, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2),
-    EMERALD(Material.IRON, MapColor.EMERALD, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2),
-    LAPIS(Material.IRON, MapColor.LAPIS, SoundType.STONE, 5.0F, 10.0F, "pickaxe", 1),
-    OBSIDIAN(Material.ROCK, MapColor.BLACK, SoundType.STONE, 50.0F, 2000.0F, "pickaxe", 3),
-    COAL(Material.ROCK, MapColor.BLACK, SoundType.STONE, 5.0F, 10.0F, "pickaxe", 0),
-    REDSTONE(Material.IRON, MapColor.TNT, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 0),
-    MISSINGNO(Material.IRON, MapColor.MAGENTA, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2),
-    CLAY(Material.GROUND, MapColor.CLAY, SoundType.GROUND, 0.6F, 0.6F, "shovel", 0),
-    DIRT(Material.GROUND, MapColor.DIRT, SoundType.GROUND, 0.5F, 0.0F, "shovel", 0),
-    GRASS(Material.GRASS, MapColor.GRASS, SoundType.PLANT, 0.6F, 0.0F, "shovel", 0),
+    IRON(Material.IRON, MapColor.IRON, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 1, () -> Item.getItemFromBlock(ModBlocks.iron_slab)),
+    GOLD(Material.IRON, MapColor.GOLD, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2, () -> Item.getItemFromBlock(ModBlocks.gold_slab)),
+    DIAMOND(Material.IRON, MapColor.DIAMOND, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2, () -> Item.getItemFromBlock(ModBlocks.diamond_slab)),
+    EMERALD(Material.IRON, MapColor.EMERALD, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2, () -> Item.getItemFromBlock(ModBlocks.emerald_slab)),
+    LAPIS(Material.IRON, MapColor.LAPIS, SoundType.STONE, 5.0F, 10.0F, "pickaxe", 1, () -> Item.getItemFromBlock(ModBlocks.lapis_slab)),
+    OBSIDIAN(Material.ROCK, MapColor.BLACK, SoundType.STONE, 50.0F, 2000.0F, "pickaxe", 3, () -> Item.getItemFromBlock(ModBlocks.obsidian_slab)),
+    COAL(Material.ROCK, MapColor.BLACK, SoundType.STONE, 5.0F, 10.0F, "pickaxe", 0, () -> Item.getItemFromBlock(ModBlocks.coal_slab)),
+    REDSTONE(Material.IRON, MapColor.TNT, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 0, () -> Item.getItemFromBlock(ModBlocks.redstone_slab)),
+    MISSINGNO(Material.IRON, MapColor.MAGENTA, SoundType.METAL, 5.0F, 10.0F, "pickaxe", 2, () -> Item.getItemFromBlock(ModBlocks.missingno_slab)),
+    CLAY(Material.GROUND, MapColor.CLAY, SoundType.GROUND, 0.6F, 0.6F, "shovel", 0, () -> Item.getItemFromBlock(ModBlocks.clay_slab)),
+    DIRT(Material.GROUND, MapColor.DIRT, SoundType.GROUND, 0.5F, 0.0F, "shovel", 0, () -> Item.getItemFromBlock(ModBlocks.dirt_slab)),
+    GRASS(Material.GRASS, MapColor.GRASS, SoundType.PLANT, 0.6F, 0.0F, "shovel", 0, () -> Item.getItemFromBlock(ModBlocks.grass_slab)),
 
-    IRONWOOD(Material.WOOD, MapColor.WOOD, SoundType.WOOD, 5.0F, 10.0F, null, 0),
-    FIERY(Material.IRON, MapColor.BLACK_STAINED_HARDENED_CLAY, SoundType.METAL, 5.0F, 10.0F, null, 0),
-    STEELEAF(Material.LEAVES, MapColor.FOLIAGE, SoundType.PLANT, 5.0F, 10.0F, null, 0),
-    ARCTIC(Material.CLOTH, MapColor.CLOTH, SoundType.CLOTH, 0.8F, 10.0F, null, 0),
-    CARMINITE(Material.CLAY, MapColor.RED, SoundType.SLIME, 0.0F, 10.0F, null, 0);
+    IRONWOOD(Material.WOOD, MapColor.WOOD, SoundType.WOOD, 5.0F, 10.0F, null, 0, () -> Item.getItemFromBlock(ModBlocks.ironwood_slab)),
+    FIERY(Material.IRON, MapColor.BLACK_STAINED_HARDENED_CLAY, SoundType.METAL, 5.0F, 10.0F, null, 0, () -> Item.getItemFromBlock(ModBlocks.fiery_slab)),
+    STEELEAF(Material.LEAVES, MapColor.FOLIAGE, SoundType.PLANT, 5.0F, 10.0F, null, 0, () -> Item.getItemFromBlock(ModBlocks.steeleaf_slab)),
+    ARCTIC(Material.CLOTH, MapColor.CLOTH, SoundType.CLOTH, 0.8F, 10.0F, null, 0, () -> Item.getItemFromBlock(ModBlocks.arctic_fur_slab)),
+    CARMINITE(Material.CLAY, MapColor.RED, SoundType.SLIME, 0.0F, 10.0F, null, 0, () -> Item.getItemFromBlock(ModBlocks.carminite_slab));
 
     public final Material material;
     public final MapColor color;
@@ -31,8 +34,9 @@ public enum NiftyBlock {
     public final float resistance;
     public final String tool;
     public final int level;
+    public final Supplier<Item> dropItem;
 
-    NiftyBlock(Material material, MapColor color, SoundType sound, float hardness, float resistance, String tool, int level) {
+    NiftyBlock(Material material, MapColor color, SoundType sound, float hardness, float resistance, String tool, int level, Supplier<Item> slabItem) {
         this.material = material;
         this.color = color;
         this.sound = sound;
@@ -40,5 +44,6 @@ public enum NiftyBlock {
         this.resistance = resistance;
         this.tool = tool;
         this.level = level;
+        this.dropItem = slabItem;
     }
 }
--- src/main/java/com/androsa/nifty/blocks/NiftySlab.java
+++ src/main/java/com/androsa/nifty/blocks/NiftySlab.java
@@ -10,19 +10,27 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Locale;
+import java.util.Random;
+import java.util.function.Supplier;
 
 public class NiftySlab extends BlockSlab implements BlockModelHelper {
 
     public static final PropertyEnum<NiftySlabEnum> VARIANT = PropertyEnum.create("variant", NiftySlabEnum.class);
 
     private final boolean isDouble;
+    private final Supplier<Item> dropItem;
 
     public NiftySlab(boolean isDouble, NiftyBlock block) {
         super(block.material, block.color);
@@ -39,6 +47,7 @@ public class NiftySlab extends BlockSlab implements BlockModelHelper {
         this.setDefaultState(state);
 
         this.isDouble = isDouble;
+        this.dropItem = block.dropItem;
     }
 
     @Override
@@ -54,6 +63,15 @@ public class NiftySlab extends BlockSlab implements BlockModelHelper {
     @Override
     public boolean isDouble() {
         return isDouble;
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return dropItem.get();
+    }
+
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+        return new ItemStack(dropItem.get());
     }
 
     @Override
