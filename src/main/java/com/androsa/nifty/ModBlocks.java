456d013cca1ea6480a1002c96563c1afd183bf9e
Andromander <cathy@feodoroff.com>
2019-07-21 14:50:15 +1000

    Add Grass Path Nifty blocks

--- src/main/java/com/androsa/nifty/ModBlocks.java
+++ src/main/java/com/androsa/nifty/ModBlocks.java
@@ -32,6 +32,7 @@ public class ModBlocks {
     public static final Block dirt_stairs      = new NiftyDirtStairs().setRegistryName("dirt_stairs");
     public static final Block grass_stairs     = new NiftyGrassStairs().setRegistryName("grass_stairs");
     public static final Block hay_stairs       = new NiftyStairs(HAY, false).setRegistryName("hay_stairs");
+    public static final Block path_stairs      = new NiftyPathStairs().setRegistryName("grass_path_stairs");
 
     public static final Block iron_slab      = new NiftySlab(IRON).setRegistryName("iron_slab");
     public static final Block gold_slab      = new NiftySlab(GOLD).setRegistryName("gold_slab");
@@ -46,6 +47,7 @@ public class ModBlocks {
     public static final Block dirt_slab      = new NiftyDirtSlab().setRegistryName("dirt_slab");
     public static final Block grass_slab     = new NiftyGrassSlab().setRegistryName("grass_slab");
     public static final Block hay_slab       = new NiftySlab(HAY).setRegistryName("hay_slab");
+    public static final Block path_slab      = new NiftyPathSlab().setRegistryName("grass_path_slab");
 
     public static final Block iron_fence      = new NiftyFence(IRON).setRegistryName("iron_fence");
     public static final Block gold_fence      = new NiftyFence(GOLD).setRegistryName("gold_fence");
@@ -60,6 +62,7 @@ public class ModBlocks {
     public static final Block dirt_fence      = new NiftyDirtFence().setRegistryName("dirt_fence");
     public static final Block grass_fence     = new NiftyGrassFence().setRegistryName("grass_fence");
     public static final Block hay_fence       = new NiftyFence(HAY).setRegistryName("hay_fence");
+    public static final Block path_fence      = new NiftyPathFence().setRegistryName("grass_path_fence");
 
     public static final Block gold_trapdoor      = new NiftyTrapDoor(GOLD).setRegistryName("gold_trapdoor");
     public static final Block diamond_trapdoor   = new NiftyTrapDoor(DIAMOND).setRegistryName("diamond_trapdoor");
@@ -73,6 +76,7 @@ public class ModBlocks {
     public static final Block dirt_trapdoor      = new NiftyDirtTrapDoor().setRegistryName("dirt_trapdoor");
     public static final Block grass_trapdoor     = new NiftyGrassTrapDoor().setRegistryName("grass_trapdoor");
     public static final Block hay_trapdoor       = new NiftyTrapDoor(HAY).setRegistryName("hay_trapdoor");
+    public static final Block path_trapdoor      = new NiftyPathTrapDoor().setRegistryName("grass_path_trapdoor");
 
     public static final Block iron_fence_gate      = new NiftyFenceGate(IRON).setRegistryName("iron_fence_gate");
     public static final Block gold_fence_gate      = new NiftyFenceGate(GOLD).setRegistryName("gold_fence_gate");
@@ -87,20 +91,21 @@ public class ModBlocks {
     public static final Block dirt_fence_gate      = new NiftyDirtFenceGate().setRegistryName("dirt_fence_gate");
     public static final Block grass_fence_gate     = new NiftyGrassFenceGate().setRegistryName("grass_fence_gate");
     public static final Block hay_fence_gate       = new NiftyFenceGate(HAY).setRegistryName("hay_fence_gate");
+    public static final Block path_fence_gate      = new NiftyPathFenceGate().setRegistryName("grass_path_fence_gate");
 
     @SubscribeEvent
     public static void onRegisterBlocks(RegistryEvent.Register<Block> e) {
         final Block blocks[] = new Block[] {
                 //Stairs
-                iron_stairs, gold_stairs, diamond_stairs, emerald_stairs, lapis_stairs, obsidian_stairs, coal_stairs, redstone_stairs, missingno_stairs, clay_stairs, dirt_stairs, grass_stairs, hay_stairs,
+                iron_stairs, gold_stairs, diamond_stairs, emerald_stairs, lapis_stairs, obsidian_stairs, coal_stairs, redstone_stairs, missingno_stairs, clay_stairs, dirt_stairs, grass_stairs, hay_stairs, path_stairs,
                 //Slabs
-                iron_slab, gold_slab, diamond_slab, emerald_slab, lapis_slab, obsidian_slab, coal_slab, redstone_slab, missingno_slab, clay_slab, dirt_slab, grass_slab, hay_slab,
+                iron_slab, gold_slab, diamond_slab, emerald_slab, lapis_slab, obsidian_slab, coal_slab, redstone_slab, missingno_slab, clay_slab, dirt_slab, grass_slab, hay_slab, path_slab,
                 //Fences
-                iron_fence, gold_fence, diamond_fence, emerald_fence, lapis_fence, obsidian_fence, coal_fence, redstone_fence, missingno_fence, clay_fence, dirt_fence, grass_fence, hay_fence,
+                iron_fence, gold_fence, diamond_fence, emerald_fence, lapis_fence, obsidian_fence, coal_fence, redstone_fence, missingno_fence, clay_fence, dirt_fence, grass_fence, hay_fence, path_fence,
                 //Trapdoors
-                gold_trapdoor, diamond_trapdoor, emerald_trapdoor, lapis_trapdoor, obsidian_trapdoor, coal_trapdoor, redstone_trapdoor, missingno_trapdoor, clay_trapdoor, dirt_trapdoor, grass_trapdoor, hay_trapdoor,
+                gold_trapdoor, diamond_trapdoor, emerald_trapdoor, lapis_trapdoor, obsidian_trapdoor, coal_trapdoor, redstone_trapdoor, missingno_trapdoor, clay_trapdoor, dirt_trapdoor, grass_trapdoor, hay_trapdoor, path_trapdoor,
                 //Fence Gates
-                iron_fence_gate, gold_fence_gate, diamond_fence_gate, emerald_fence_gate, lapis_fence_gate, obsidian_fence_gate, coal_fence_gate, redstone_fence_gate, missingno_fence_gate, clay_fence_gate, dirt_fence_gate, grass_fence_gate, hay_fence_gate
+                iron_fence_gate, gold_fence_gate, diamond_fence_gate, emerald_fence_gate, lapis_fence_gate, obsidian_fence_gate, coal_fence_gate, redstone_fence_gate, missingno_fence_gate, clay_fence_gate, dirt_fence_gate, grass_fence_gate, hay_fence_gate, path_fence_gate
         };
 
         e.getRegistry().registerAll(blocks);
@@ -123,6 +128,7 @@ public class ModBlocks {
         registry.register(registerBlock(dirt_stairs, ItemGroup.BUILDING_BLOCKS));
         registry.register(registerBlock(grass_stairs, ItemGroup.BUILDING_BLOCKS));
         registry.register(registerBlock(hay_stairs, ItemGroup.BUILDING_BLOCKS));
+        registry.register(registerBlock(path_stairs, ItemGroup.BUILDING_BLOCKS));
 
         registry.register(registerBlock(iron_slab, ItemGroup.BUILDING_BLOCKS));
         registry.register(registerBlock(gold_slab, ItemGroup.BUILDING_BLOCKS));
@@ -137,6 +143,7 @@ public class ModBlocks {
         registry.register(registerBlock(dirt_slab, ItemGroup.BUILDING_BLOCKS));
         registry.register(registerBlock(grass_slab, ItemGroup.BUILDING_BLOCKS));
         registry.register(registerBlock(hay_slab, ItemGroup.BUILDING_BLOCKS));
+        registry.register(registerBlock(path_slab, ItemGroup.BUILDING_BLOCKS));
 
         registry.register(registerBlock(iron_fence, ItemGroup.DECORATIONS));
         registry.register(registerBlock(gold_fence, ItemGroup.DECORATIONS));
@@ -151,6 +158,7 @@ public class ModBlocks {
         registry.register(registerBlock(dirt_fence, ItemGroup.DECORATIONS));
         registry.register(registerBlock(grass_fence, ItemGroup.DECORATIONS));
         registry.register(registerBlock(hay_fence, ItemGroup.DECORATIONS));
+        registry.register(registerBlock(path_fence, ItemGroup.DECORATIONS));
 
         registry.register(registerBlock(gold_trapdoor, ItemGroup.REDSTONE));
         registry.register(registerBlock(diamond_trapdoor, ItemGroup.REDSTONE));
@@ -164,6 +172,7 @@ public class ModBlocks {
         registry.register(registerBlock(dirt_trapdoor, ItemGroup.REDSTONE));
         registry.register(registerBlock(grass_trapdoor, ItemGroup.REDSTONE));
         registry.register(registerBlock(hay_trapdoor, ItemGroup.REDSTONE));
+        registry.register(registerBlock(path_trapdoor, ItemGroup.REDSTONE));
 
         registry.register(registerBlock(iron_fence_gate, ItemGroup.REDSTONE));
         registry.register(registerBlock(gold_fence_gate, ItemGroup.REDSTONE));
@@ -178,6 +187,7 @@ public class ModBlocks {
         registry.register(registerBlock(dirt_fence_gate, ItemGroup.REDSTONE));
         registry.register(registerBlock(grass_fence_gate, ItemGroup.REDSTONE));
         registry.register(registerBlock(hay_fence_gate, ItemGroup.REDSTONE));
+        registry.register(registerBlock(path_fence_gate, ItemGroup.REDSTONE));
     }
 
     private static Item registerBlock(Block block, ItemGroup group) {
---
+++ src/main/java/com/androsa/nifty/blocks/NiftyPathFence.java
@@ -0,0 +1,62 @@
+package com.androsa.nifty.blocks;
+
+import com.androsa.nifty.ModBlocks;
+import com.androsa.nifty.NiftyBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class NiftyPathFence extends NiftyFence {
+
+    protected final VoxelShape[] collisionShapes;
+    protected final VoxelShape[] shapes;
+
+    public NiftyPathFence() {
+        super(NiftyBlock.PATH);
+
+        this.collisionShapes = this.makeShapes(2.0F, 2.0F, 23.0F, 0.0F, 23.0F);
+        this.shapes = this.makeShapes(2.0F, 2.0F, 15.0F, 0.0F, 15.0F);
+    }
+
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return this.shapes[this.getIndex(state)];
+    }
+
+    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return this.collisionShapes[this.getIndex(state)];
+    }
+
+    @Override
+    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity playerIn, Hand hand, BlockRayTraceResult result) {
+        ItemStack itemstack = playerIn.getHeldItem(hand);
+
+        if (!itemstack.isEmpty() && itemstack.getItem() instanceof HoeItem) {
+            BlockState blockstate =  worldIn.getBlockState(pos);
+            worldIn.setBlockState(pos, ModBlocks.grass_fence.getDefaultState().with(NORTH, blockstate.get(NORTH)).with(SOUTH, blockstate.get(SOUTH)).with(EAST, blockstate.get(EAST)).with(WEST, blockstate.get(WEST)).with(WATERLOGGED, blockstate.get(WATERLOGGED)), 3);
+            worldIn.playSound(null, pos, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            itemstack.damageItem(1, playerIn, (user) -> user.sendBreakAnimation(hand));
+            return true;
+        } else {
+            return super.onBlockActivated(state, worldIn, pos, playerIn, hand, result);
+        }
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+}
