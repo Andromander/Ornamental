b3cb6fa8abbf81d625eb397a6b5f8cefd0112457
Andromander <cathy@feodoroff.com>
2020-10-22 21:49:56 +1000

    Generate recipes for Poles and Beams
    Includes conversion recipe

--- src/main/java/com/androsa/ornamental/data/provider/OrnamentalRecipeProvider.java
+++ src/main/java/com/androsa/ornamental/data/provider/OrnamentalRecipeProvider.java
@@ -9,6 +9,7 @@ import net.minecraft.block.SlabBlock;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.data.ShapelessRecipeBuilder;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.crafting.ConditionalRecipe;
@@ -140,5 +141,59 @@ public abstract class OrnamentalRecipeProvider extends ForgeRecipeProvider imple
                         .addCriterion("has_" + builder.name, hasItem(ingredient))
                         ::build)
                 .build(consumer, loc(builder.name + "_door"));
+    }
+
+    public void pole(Consumer<IFinishedRecipe> consumer, Supplier<? extends OrnamentPole> result, Supplier<? extends OrnamentSlab> ingredient) {
+        pole(consumer, result, ingredient.get());
+    }
+
+    public void pole(Consumer<IFinishedRecipe> consumer, Supplier<? extends OrnamentPole> result, IItemProvider ingredient) {
+        OrnamentBuilder builder = result.get().getBuilder();
+        ConditionalRecipe.builder()
+                .addCondition(new ConfigCondition(builder.booleanValue.get().getPath().get(0)))
+                .addRecipe(ShapedRecipeBuilder.shapedRecipe(result.get(), 6)
+                        .patternLine("#")
+                        .patternLine("#")
+                        .patternLine("#")
+                        .key('#', ingredient)
+                        .addCriterion("has_" + builder.name, hasItem(ingredient))
+                        ::build)
+                .build(consumer, loc(builder.name + "_pole"));
+    }
+
+    public void beam(Consumer<IFinishedRecipe> consumer, Supplier<? extends OrnamentBeam> result, Supplier<? extends OrnamentSlab> ingredient) {
+        beam(consumer, result, ingredient.get());
+    }
+
+    public void beam(Consumer<IFinishedRecipe> consumer, Supplier<? extends OrnamentBeam> result, IItemProvider ingredient) {
+        OrnamentBuilder builder = result.get().getBuilder();
+        ConditionalRecipe.builder()
+                .addCondition(new ConfigCondition(builder.booleanValue.get().getPath().get(0)))
+                .addRecipe(ShapedRecipeBuilder.shapedRecipe(result.get(), 6)
+                        .patternLine("###")
+                        .key('#', ingredient)
+                        .addCriterion("has_" + builder.name, hasItem(ingredient))
+                        ::build)
+                .build(consumer, loc(builder.name + "_beam"));
+    }
+
+    public void convertPoleBeam(Consumer<IFinishedRecipe> consumer, Supplier<? extends OrnamentPole> pole, Supplier<? extends OrnamentBeam> beam) {
+        OrnamentBuilder polebuilder = pole.get().getBuilder();
+        OrnamentBuilder beambuilder = beam.get().getBuilder();
+
+        ConditionalRecipe.builder()
+                .addCondition(new ConfigCondition(polebuilder.booleanValue.get().getPath().get(0)))
+                .addRecipe(ShapelessRecipeBuilder.shapelessRecipe(beam.get())
+                        .addIngredient(pole.get())
+                        .addCriterion("has_" + polebuilder.name, hasItem(pole.get()))
+                        ::build)
+                .build(consumer, loc(polebuilder.name + "_pole_to_beam"));
+        ConditionalRecipe.builder()
+                .addCondition(new ConfigCondition(beambuilder.booleanValue.get().getPath().get(0)))
+                .addRecipe(ShapelessRecipeBuilder.shapelessRecipe(pole.get())
+                        .addIngredient(beam.get())
+                        .addCriterion("has_" + beambuilder.name, hasItem(beam.get()))
+                        ::build)
+                .build(consumer, loc(beambuilder.name + "_beam_to_pole"));
     }
 }
