4ed049c95e320fd4d8608e2ba7b8e4d3a80335ec
Andromander <androsa@protonmail.com>
2022-07-27 23:16:43 +1000

    Clean up item model generator

--- src/main/java/com/androsa/ornamental/data/provider/OrnamentalItemModelProvider.java
+++ src/main/java/com/androsa/ornamental/data/provider/OrnamentalItemModelProvider.java
@@ -1,9 +1,9 @@
 package com.androsa.ornamental.data.provider;
 
-import com.androsa.ornamental.OrnamentalMod;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,108 +16,143 @@ public abstract class OrnamentalItemModelProvider extends ItemModelProvider {
         super(generator, modid, helper);
     }
 
-    public ResourceLocation ornamentLoc(String path) {
-        return new ResourceLocation(OrnamentalMod.MODID, path);
-    }
-
     public String blockName(Supplier<? extends Block> block) {
         return ForgeRegistries.ITEMS.getKey(block.get().asItem()).getPath();
     }
 
     public void blockItem(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), modLoc("block/" + blockName(block)));
+        existingParent(block, modLoc("block/" + blockName(block)));
     }
 
     public void blockItemModel(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), mcLoc("item/generated"))
+        existingParent(block, mcLoc("item/generated"))
                 .texture("layer0", modLoc("item/" + blockName(block)));
     }
 
     public void blockItemModel(Supplier<? extends Block> block, String name) {
-        withExistingParent(blockName(block), mcLoc("item/generated"))
+        existingParent(block, mcLoc("item/generated"))
                 .texture("layer0", modLoc("item/" + name));
     }
 
     public void blockItemFence(Supplier<? extends Block> block, String name) {
-        withExistingParent(blockName(block), mcLoc("block/fence_inventory"))
-                .texture("texture", mcLoc("block/" + name));
+        ResourceLocation loc = mcLoc("block/" + name);
+        blockItemFence(block, loc, loc, loc);
     }
 
-    public void blockItemFenceColumn(Supplier<? extends Block> block, String side, String top) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/fence_inventory_column"))
-                .texture("end", mcLoc("block/" + top))
-                .texture("side", mcLoc("block/" + side));
+    public void blockItemFence(Supplier<? extends Block> block, String side, String top) {
+        ResourceLocation sideloc = mcLoc("block/" + side);
+        ResourceLocation toploc = mcLoc("block/" + top);
+        blockItemFence(block, sideloc, toploc, toploc);
+    }
+
+    public void blockItemFence(Supplier<? extends Block> block, ResourceLocation side, ResourceLocation top, ResourceLocation bottom) {
+        existingParent(block, modLoc("block/util/fence_inventory"))
+                .texture("side", side)
+                .texture("top", top)
+                .texture("bottom", bottom);
     }
 
     public void blockItemFenceMissing(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), mcLoc("block/fence_inventory"))
-                .texture("texture", modLoc("block/missingno"));
+        ResourceLocation tex = modLoc("block/missingno");
+        blockItemFence(block, tex, tex, tex);
     }
 
     public void blockItemTrapdoor(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), modLoc("block/" + blockName(block) + "_bottom"));
+        existingParent(block, modLoc("block/" + blockName(block) + "_bottom"));
     }
 
     public void blockItemPole(Supplier<? extends Block> block, String name) {
-        blockItemPole(block, name, name);
+        ResourceLocation tex = mcLoc("block/" + name);
+        blockItemPole(block, tex, tex, tex);
     }
 
     public void blockItemPole(Supplier<? extends Block> block, String end, String side) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/pole_inventory"))
-                .texture("end", mcLoc("block/" + end))
-                .texture("side", mcLoc("block/" + side));
+        ResourceLocation sideloc = mcLoc("block/" + side);
+        ResourceLocation toploc = mcLoc("block/" + end);
+        blockItemPole(block, toploc, toploc, sideloc);
+    }
+
+    public void blockItemPole(Supplier<? extends Block> block, ResourceLocation top, ResourceLocation bottom, ResourceLocation side) {
+        existingParent(block, modLoc("block/util/pole_inventory"))
+                .texture("side", side)
+                .texture("top", top)
+                .texture("bottom", bottom);
     }
 
     public void blockItemPoleMissing(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/pole_inventory"))
-                .texture("end", modLoc("block/missingno"))
-                .texture("side", modLoc("block/missingno"));
+        ResourceLocation tex = modLoc("block/missingno");
+        blockItemPole(block, tex, tex, tex);
     }
 
     public void blockItemBeam(Supplier<? extends Block> block, String name) {
-        blockItemBeam(block, name, name);
+        ResourceLocation tex = mcLoc("block/" + name);
+        blockItemBeam(block, tex, tex, tex);
     }
 
     public void blockItemBeam(Supplier<? extends Block> block, String end, String side) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/beam_inventory"))
-                .texture("end", mcLoc("block/" + end))
-                .texture("side", mcLoc("block/" + side));
+        ResourceLocation sideloc = mcLoc("block/" + side);
+        ResourceLocation toploc = mcLoc("block/" + end);
+        blockItemBeam(block, toploc, toploc, sideloc);
+    }
+
+    public void blockItemBeam(Supplier<? extends Block> block, ResourceLocation top, ResourceLocation bottom, ResourceLocation side) {
+        existingParent(block, modLoc("block/util/beam_inventory"))
+                .texture("side", side)
+                .texture("top", top)
+                .texture("bottom", bottom);
     }
 
     public void blockItemBeamMissing(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/beam_inventory"))
-                .texture("end", modLoc("block/missingno"))
-                .texture("side", modLoc("block/missingno"));
+        ResourceLocation tex = modLoc("block/missingno");
+        blockItemBeam(block, tex, tex, tex);
     }
 
     public void blockItemWall(Supplier<? extends Block> block, String name) {
-        withExistingParent(blockName(block), mcLoc("block/wall_inventory"))
-                .texture("wall", mcLoc("block/" + name));
+        ResourceLocation tex = mcLoc("block/" + name);
+        blockItemWall(block, tex, tex, tex);
     }
 
-    public void blockItemWallColumn(Supplier<? extends Block> block, String side, String top) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/wall_inventory_column"))
-                .texture("end", mcLoc("block/" + top))
-                .texture("side", mcLoc("block/" + side));
+    public void blockItemWall(Supplier<? extends Block> block, String side, String top) {
+        ResourceLocation sideloc = mcLoc("block/" + side);
+        ResourceLocation toploc = mcLoc("block/" + top);
+        blockItemWall(block, sideloc, toploc, toploc);
+    }
+
+    public void blockItemWall(Supplier<? extends Block> block, ResourceLocation side, ResourceLocation top, ResourceLocation bottom) {
+        existingParent(block, modLoc("block/util/wall_inventory"))
+                .texture("side", side)
+                .texture("top", top)
+                .texture("bottom", bottom);
     }
 
     public void blockItemWallMissing(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), mcLoc("block/wall_inventory"))
-                .texture("wall", modLoc("block/missingno"));
+        ResourceLocation tex = modLoc("block/missingno");
+        blockItemWall(block, tex, tex, tex);
     }
 
     public void blockItemSaddleDoor(Supplier<? extends Block> block, String name) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/saddle_door_inventory"))
-                .texture("texture", modLoc("block/" + name + "_trapdoor"));
+        ResourceLocation texture = modLoc("block/" + name + "_trapdoor");
+        blockItemSaddleDoor(block, texture, texture, texture);
     }
 
     public void blockItemSaddleDoorV(Supplier<? extends Block> block, String name) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/saddle_door_inventory"))
-                .texture("texture", mcLoc("block/" + name));
+        ResourceLocation texture = mcLoc("block/" + name);
+        blockItemSaddleDoor(block, texture, texture, texture);
     }
 
     public void blockItemSaddleDoorM(Supplier<? extends Block> block) {
-        withExistingParent(blockName(block), ornamentLoc("block/util/saddle_door_inventory"))
-                .texture("texture", modLoc("block/missingno"));
+        ResourceLocation texture = modLoc("block/missingno");
+        blockItemSaddleDoor(block, texture, texture, texture);
+    }
+
+    public void blockItemSaddleDoor(Supplier<? extends Block> block, ResourceLocation side, ResourceLocation top, ResourceLocation bottom) {
+        existingParent(block, modLoc("block/util/saddle_door_inventory"))
+                .texture("side", side)
+                .texture("top", top)
+                .texture("bottom", bottom);
+    }
+
+    public ItemModelBuilder existingParent(Supplier<? extends Block> block, ResourceLocation parent) {
+        return withExistingParent(blockName(block), parent);
     }
 }
