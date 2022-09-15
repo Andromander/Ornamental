9a2c176ff07466e868b8e8f71cb27611b1db6686
Andromander <androsa@protonmail.com>
2022-09-15 21:42:47 +1000

    Create override recipe generators

--- src/main/java/com/androsa/ornamental/data/provider/OrnamentalRecipeProvider.java
+++ src/main/java/com/androsa/ornamental/data/provider/OrnamentalRecipeProvider.java
@@ -2,6 +2,7 @@ package com.androsa.ornamental.data.provider;
 
 import com.androsa.ornamental.builder.OrnamentBuilder;
 import com.androsa.ornamental.blocks.*;
+import com.androsa.ornamental.registry.ModTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
@@ -9,6 +10,7 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.function.Consumer;
@@ -37,6 +39,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_stairs");
     }
 
+    /*
+     * In the event a Stair recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void stairsOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentStair> result, Block ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 8)
+                .pattern("# /")
+                .pattern("## ")
+                .pattern("###")
+                .define('/', ItemTags.STAIRS)
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_stairs");
+    }
+
     public void slab(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentSlab> result, Block ingredient) {
         ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 6)
                 .pattern("###")
@@ -45,6 +61,9 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_slab");
     }
 
+    /*
+     * In the event a Slab recipe is conflicting with another, this is almost guaranteed to work
+     */
     public void slabOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentSlab> result, Block ingredient) {
 		ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 6)
 				.pattern(" / ")
@@ -69,6 +88,21 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), bigItem, "_fence");
     }
 
+    /*
+     * In the event a Fence recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void fenceOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentFence> result, Block bigItem, ItemLike smallItem) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 3)
+                .pattern(" * ")
+                .pattern("#/#")
+                .pattern("#/#")
+                .define('*', ItemTags.FENCES)
+                .define('#', bigItem)
+                .define('/', smallItem);
+
+        internalRecipeBuild(consumer, recipe, result.get(), bigItem, "_fence");
+    }
+
     public void trapdoor(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentTrapDoor> result, Supplier<? extends OrnamentSlab> ingredient) {
         trapdoor(consumer, result, ingredient.get());
     }
@@ -77,6 +111,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
                 .pattern("##")
                 .pattern("##")
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_trapdoor");
+    }
+
+    /*
+     * In the event a Trapdoor recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void trapdoorOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentTrapDoor> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
+                .pattern(" /")
+                .pattern("##")
+                .pattern("##")
+                .define('/', ItemTags.TRAPDOORS)
                 .define('#', ingredient);
 
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_trapdoor");
@@ -95,6 +143,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_trapdoor");
     }
 
+    /*
+     * In the event a wide Trapdoor recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void trapdoorWideOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentTrapDoor> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
+                .pattern(" / ")
+                .pattern("###")
+                .pattern("###")
+                .define('/', ItemTags.TRAPDOORS)
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_trapdoor");
+    }
+
     public void fencegate(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentFenceGate> result, Block bigItem, Supplier<? extends OrnamentSlab> smallItem) {
         fencegate(consumer, result, bigItem, smallItem.get());
     }
@@ -103,6 +165,21 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
                 .pattern("/#/")
                 .pattern("/#/")
+                .define('#', bigItem)
+                .define('/', smallItem);
+
+        internalRecipeBuild(consumer, recipe, result.get(), bigItem, "_fence_gate");
+    }
+
+    /*
+     * In the event a Fence Gate recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void fencegateOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentFenceGate> result, Block bigItem, ItemLike smallItem) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
+                .pattern(" * ")
+                .pattern("/#/")
+                .pattern("/#/")
+                .define('*', Tags.Items.FENCE_GATES)
                 .define('#', bigItem)
                 .define('/', smallItem);
 
@@ -123,6 +200,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_door");
     }
 
+    /*
+     * In the event a Door recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void doorOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentDoor> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
+                .pattern("## ")
+                .pattern("##/")
+                .pattern("## ")
+                .define('/', ItemTags.DOORS)
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_door");
+    }
+
     public void pole(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentPole> result, Supplier<? extends OrnamentSlab> ingredient) {
         pole(consumer, result, ingredient.get());
     }
@@ -137,6 +228,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_pole");
     }
 
+    /*
+     * This wouldn't be necessary, but in the event this recipe shape is occupied, this exists
+     */
+    public void poleOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentPole> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 6)
+                .pattern("# ")
+                .pattern("#/")
+                .pattern("# ")
+                .define('/', ModTags.Items.POLES)
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_pole");
+    }
+
     public void beam(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentBeam> result, Supplier<? extends OrnamentSlab> ingredient) {
         beam(consumer, result, ingredient.get());
     }
@@ -144,6 +249,19 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
     public void beam(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentBeam> result, ItemLike ingredient) {
         ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 6)
                 .pattern("###")
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_beam");
+    }
+
+    /*
+     * This wouldn't be necessary, but in the event this recipe shape is occupied, this exists
+     */
+    public void beamOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentBeam> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 6)
+                .pattern(" / ")
+                .pattern("###")
+                .define('/', ModTags.Items.BEAMS)
                 .define('#', ingredient);
 
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_beam");
@@ -168,6 +286,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_wall");
     }
 
+    /*
+     * In the event a Wall recipe is conflicting with another, this is almost guaranteed to work
+     */
+    public void wallOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentWall> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get())
+                .pattern(" / ")
+                .pattern("###")
+                .pattern("###")
+                .define('/', ItemTags.WALLS)
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_wall");
+    }
+
     public void saddleDoor(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentSaddleDoor> result, Supplier<? extends OrnamentTrapDoor> ingredient) {
         saddleDoor(consumer, result, ingredient.get());
     }
@@ -176,6 +308,20 @@ public abstract class OrnamentalRecipeProvider extends RecipeProvider implements
         ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 2)
                 .pattern("#")
                 .pattern("#")
+                .define('#', ingredient);
+
+        internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_saddle_door");
+    }
+
+    /*
+     * This wouldn't be necessary, but in the event this recipe shape is occupied, this exists
+     */
+    public void saddleDoorOverride(Consumer<FinishedRecipe> consumer, Supplier<? extends OrnamentSaddleDoor> result, ItemLike ingredient) {
+        ShapedRecipeBuilder recipe = ShapedRecipeBuilder.shaped(result.get(), 2)
+                .pattern(" / ")
+                .pattern("#")
+                .pattern("#")
+                .define('/', ModTags.Items.SADDLE_DOORS)
                 .define('#', ingredient);
 
         internalRecipeBuild(consumer, recipe, result.get(), ingredient, "_saddle_door");
